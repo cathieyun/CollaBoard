1a4962f90382b4d1a669ef26264093b9f4232823
Cathie Yun <cathieyun@gmail.com>
2013-12-08 23:18:38 -0500

    fixed OvalTest

--- src/canvas/OvalTest.java
+++ src/canvas/OvalTest.java
@@ -15,14 +15,16 @@ public class OvalTest {
 		assertEquals(oval1.toString(), oval2.toString());
 	}
 	//Test that an oval is an instance of a DrawingObject
+	@Test
 	public void instanceOfDrawingObjectTest(){
 		Oval oval1 = new Oval(5, 5, 20, 26, "blk", "m");
 		assertEquals(true, oval1 instanceof DrawingObject);
 	}
 	//Test that the toString method of Oval works properly, returns all info.
+	@Test
 	public void toStringTest(){
 		Oval oval1 = new Oval(5, 5, 20, 26, "blk", "m");
-		assertEquals(oval1.toString(),"oval 5 5 20 26 black thin");
+		assertEquals(oval1.toString(),"oval 5 5 20 26 blk m");
 	}
 	
 }
